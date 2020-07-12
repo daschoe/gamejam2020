ce61010a001ae3b276db8594c7ae0954fdb276a9
Berinrin <b7online@yahoo.com>
2020-07-12 23:46:33 +0200

    Added money
    Added currency to game manager and money pane to inventory UI

---
+++ GameJam2020/Assets/Scripts/Currency.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Currency : MonoBehaviour
+{
+    public int currency;
+    public int maxCurrency = 9999;
+    GameObject currencyUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currencyUI = GameObject.Find("Currency");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currencyUI.GetComponent<Text>().text = currency.ToString();
+        if (currency < 0)
+          currency = 0;
+    }
+}
