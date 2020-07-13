16df5c87a75872417215244471e817c45e2c8754
daschoe <thelibrarian.ls@gmail.com>
2020-07-13 21:34:44 +0200

    Walking anmiation structure finished.

--- GameJam2020/Assets/Scripts/PlayerControl.cs
+++ GameJam2020/Assets/Scripts/PlayerControl.cs
@@ -6,6 +6,8 @@ public class PlayerControl : MonoBehaviour
 {
 	public float moveSpeed;
     private Animator anim;
+    private bool playerMoving; //switch between idle and walking
+    private Vector2 lastMove;
 	
     // Start is called before the first frame update
     void Start()
@@ -16,18 +18,27 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerMoving = false;
+
         if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
             //move right or left
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
         else if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
             //move up or down
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
 
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMove", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
     }
 }
