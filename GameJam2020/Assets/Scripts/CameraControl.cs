e0adb263dd8f3e178aca70a5414f38a18c6243ca
Berinrin <b7online@yahoo.com>
2020-07-19 22:03:10 +0200

    The camera does go beyond the dge of the map anymore
    added trigger colliders to the edge of the map that keep the camera from following the player along the respective axis. also added colliders to the npc houses

--- GameJam2020/Assets/Scripts/CameraControl.cs
+++ GameJam2020/Assets/Scripts/CameraControl.cs
@@ -7,17 +7,18 @@ public class CameraControl : MonoBehaviour
 	public GameObject followTarget;
 	private Vector3 targetPosition;
 	public float moveSpeed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public bool LockX = false;
+	public bool LockY = false;
+	private float x;
+	private float y;
 
     // Update is called once per frame
     void Update()
     {
-        targetPosition = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
-        transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+					x = LockX == true ? transform.position.x : followTarget.transform.position.x;
+					y = LockY == true ? transform.position.y : followTarget.transform.position.y;
+					targetPosition = new Vector3(x, y, transform.position.z);
+					transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
     }
+
 }
---
+++ GameJam2020/Assets/Scripts/LockCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCamera : MonoBehaviour
+{
+    [SerializeField]
+    public CameraControl cameraControl;
+    public string axis;
+    // Start is called before the first frame update
+
+    void OnTriggerEnter2D (Collider2D col)
+		{
+			if (col.tag == "Player")
+			{
+        if (axis == "x")
+				    cameraControl.LockX = true;
+        else if (axis == "y")
+            cameraControl.LockY = true;
+			}
+		}
+
+		void OnTriggerExit2D (Collider2D col)
+		{
+			if (col.tag == "Player")
+			{
+        if (axis == "x")
+            cameraControl.LockX = false;
+        else if (axis == "y")
+            cameraControl.LockY = false;
+			}
+		}
+}
