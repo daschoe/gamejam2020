7e6ac0b3253319ba3d67e1db356a80a3cf6e8d31
Berinrin <b7online@yahoo.com>
2020-07-22 00:12:57 +0200

    added plant growth (still buggy)

--- GameJam2020/Assets/Scripts/Farming/PlantGrowth.cs
+++ GameJam2020/Assets/Scripts/Farming/PlantGrowth.cs
@@ -2,19 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Plantgrowth : MonoBehaviour
+public class PlantGrowth : MonoBehaviour
 {
   public CropSeed crop;
+  public Sprite seed;
+  public Sprite seedling;
+  public Sprite smallPlant;
+  public Sprite plant;
+  private int dayCounter;
 
     // Start is called before the first frame update
     void Start()
     {
-
+      dayCounter = 0;
+      this.GetComponent<SpriteRenderer>().sprite = seed;
+      DaysCounter.instance.onDayChangedCallback += UpdatePlant;
     }
-
-    // Update is called once per frame
-    void Update()
+    //Update plant status on day change, spawn crop on final day
+    void UpdatePlant ()
     {
-
+      dayCounter++;
+      if (dayCounter > crop.GrowthTime)
+      {
+        Debug.Log("Spawn Item: "+ crop.crop);
+        Instantiate(crop.crop,transform.position,Quaternion.identity,transform);
+        //Destroy(gameObject);
+      } else
+      {
+        if ( dayCounter == 1)
+        {
+          this.GetComponent<SpriteRenderer>().sprite = seedling;
+        } else if (dayCounter < (crop.GrowthTime/2))
+          {
+            this.GetComponent<SpriteRenderer>().sprite = smallPlant;
+          } else {
+            this.GetComponent<SpriteRenderer>().sprite = plant;
+          }
+      }
     }
+
 }
--- GameJam2020/Assets/Scripts/Item/CropSeed.cs
+++ GameJam2020/Assets/Scripts/Item/CropSeed.cs
@@ -6,6 +6,7 @@ public class CropSeed : Item
 {
     [Range(1,5)]
     public int GrowthTime;
+    public Item crop;
     public PlayerControl player;
     //when used on plot, spawn plant
     public override void Use()
@@ -16,6 +17,7 @@ public class CropSeed : Item
         Debug.Log("Planting " + name);
         Inventory.instance.Remove(this);
         GameObject newPlant = (GameObject)Instantiate(Resources.Load("Plant"));
+        newPlant.gameObject.GetComponent<PlantGrowth>().crop = this;
         Instantiate(newPlant, player.currentSpot.transform.position,Quaternion.identity);
         player.ReduceStamina(10);
       }
